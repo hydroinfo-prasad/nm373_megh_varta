bf0803d1c4cfe8cea8c1871de3d33720f1821e6c
deepika-sketch <68155715+deepika-sketch@users.noreply.github.com>
2020-08-02 16:10:52 +0530

    Add files via upload

--- conv_lstm/pre_process.py
+++ conv_lstm/pre_process.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 
-def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img_path,inp_seq_len=3,pred_frame=1,normalized_output='no',Validation_split=None):
+def Load_additional_data(path,start_img_path, end_img_path):
     #path = "/home/prasad/SIH/oct/tir/" 
     files = os.listdir( path )
     files=sorted(files)
@@ -46,27 +46,37 @@ def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img
         
         X.append(img)
         i=i+1
+    print('converting output into numpy arrays')    
     X=np.array(X)
+    time=np.array(time)
+    date=np.array(date)
+    month=np.array(month)
+    images_path=np.array(images_path)
+    return(X,time,date,month,images_path)
+    
+def Preprocessed_additional_data(X,time,date,month,images_path,normalization_type='scaling',inp_seq_len=3,pred_frame=1,normalized_output='no',Validation_split=None):
     
     #print(mean,std_dev)
     window_size=inp_seq_len  #10
     
+    '''
     if(Validation_split!=None):
-        num_inp = len(X)-window_size
+        num_sq = len(X)-window_size
         #print(num_inp)
-        ind = int(num_inp*Validation_split)
+        ind = int(num_sq*(1-Validation_split))
         if ind!=0:
-            mean, std_dev = (X[:ind]).mean(),(X[:ind]).std()
-            print('dataset is too small for validation set')
+            mean, std_dev = (X[:ind+window_size]).mean(),(X[:ind+window_size]).std()
+            
         else:
             mean, std_dev = X.mean(),X.std()
+            print('dataset is too small for validation set')
             
     else:
         mean, std_dev = X.mean(),X.std()
 
     Max,Min= np.max(X),np.min(X)
-    
-    
+    '''
+    '''
     print('normalizing images')
    
     i=0
@@ -82,15 +92,12 @@ def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img
                 print(i,"images out of", len(X), "are normalized")
         i=i+1
      
-            
+    '''        
     
     print('making images sequences with length:', window_size)
     
    
-    if normalized_output=='yes':
-        output='norm'
-    else:
-        output='not_normalized'
+    
     
     verify=[]
     tir_x=[]
@@ -105,7 +112,8 @@ def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img
 
         chk=0
         for j in range(i,i+window_size+pred_frame-1):
-            if ((time[j+1]==time[j]+0.5) and (date[j]==date[j+1])) or ((time[j]==23.5 and time[j+1]==0.0) and(date[j+1]==date[j]+1)):
+            if ((time[j+1]==time[j]+0.5) and (date[j]==date[j+1])) or (((time[j]==23.5 and time[j+1]==0.0) and(date[j+1]==date[j]+1))or
+                                                                      ((time[j]==23.5 and time[j+1]==0.0) and(month[j+1]!=month[j]))):
                 chk= chk+1
                 #print(i,j,chk)
 
@@ -118,12 +126,15 @@ def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img
 
             path=images_path[i:i+window_size+pred_frame]
             
+            y=X[i+window_size:i+window_size+pred_frame]
+            
+            '''
             if output=='norm':
                 y=normalised_X[i+window_size:i+window_size+pred_frame]
             else:
                 y=X[i+window_size:i+window_size+pred_frame]
+            '''
             
-            y=X[i+window_size:i+window_size+pred_frame]
             #print(i)
             tir_x.append(x)
             tir_y.append(y)
@@ -131,11 +142,72 @@ def Preprocessed_additional_data(path,normalization_type,start_img_path, end_img
         if i%50==0:
             if i>0:
                 print(i,"images out of", len(X), "are processed")
-            
+                
     tir_x=np.array(tir_x)
-    tir_y=np.array(tir_y)
-    #print(tir_x.shape)
+    tir_y=np.array(tir_y)            
+    
+    if normalization_type=='scaling':
+        print('calculating min and max')
+    else:
+        print('calculating mean and std dev')
+              
+    xf= tir_x[0]
+    
+    x_array=[]
+    for elem in xf:
+        x_array.append(elem)
+    for m in range(len(tir_y)):
+        for f in range(pred_frame):
+            x_array.append(tir_y[m][f])  
+    x_array=np.array(x_array)  
+    
+    
+    if(Validation_split!=None):
+        num_sq = len(tir_y)
+        #print(num_inp)
+        ind = int(num_sq*(1-Validation_split))
+        
+        if ind!=0:                             
+            mean, std_dev = (x_array[:ind+window_size]).mean(),(x_array[:ind+window_size]).std()
+            Max,Min= np.max(x_array[:ind+window_size]),np.min(x_array[:ind+window_size])
+            
+        else:
+            mean, std_dev = x_array.mean(),x_array.std()
+            Max,Min= np.max(x_array),np.min(x_array)
+            print('dataset is too small for validation set')
+            
+    else:
+        mean, std_dev = x_array.mean(),x_array.std()
 
+    Max,Min= np.max(x_array),np.min(x_array)            
+    
+   
+    print('normalizing images')
+    if normalized_output=='yes':
+        output='norm'
+    else:
+        output='not_normalized'
+    
+    
+    seq_len=tir_x.shape[1]
+    samples=tir_x.shape[0]
+    pred_frames=tir_y.shape[1]
+    for k in range(samples):
+        for l in range(seq_len):
+            if normalization_type=='scaling':
+                tir_x[k][l]=((tir_x[k][l])-Min)/(Max-Min)
+            if normalization_type=='zscore':
+                tir_x[k][l] = ((tir_x[k][l] - mean)/std_dev)
+        if output=='norm':        
+            for j in range(pred_frame):        
+                if normalization_type=='scaling':
+                    tir_y[k][l]= (tir_y[k][l]-Min)/(Max-Min)            
+                if normalization_type=='zscore':
+                    tir_y[k][l] = ((tir_y[k][l] - mean)/std_dev)
+        
+        
+    #print(tir_x.shape)
+    print('reshaping')
     tir_x=np.reshape(tir_x, (tir_x.shape[0],tir_x.shape[1],tir_x.shape[2],tir_x.shape[3],1))  
     if pred_frame>1:
         tir_y=np.reshape(tir_y, (tir_y.shape[0],y.shape[1],tir_y.shape[2],tir_y.shape[3],1))  
@@ -236,7 +308,7 @@ def read1day_data(dir_path,inp_seq_len=3,pred_frame=1,normalized_output='no'):
     
     return(X,y,verify)
 
-def Preprocessed_1day_data(dir_path,normalization_type,inp_seq_len=3,pred_frame=1,normalized_output='no'):
+def Preprocessed_1day_data(dir_path,normalization_type,inp_seq_len=3,pred_frame=1,normalized_output='no',Mean=0,std=0,Max=0,Min=0):
     #dir_path ="./../pytorch-unet/INSAT3D_TIR1_India/" 
     files_list=os.listdir(dir_path)
     files_list= sorted(files_list)
@@ -253,7 +325,10 @@ def Preprocessed_1day_data(dir_path,normalization_type,inp_seq_len=3,pred_frame=
         
     files=np.array(files)
     
-    mean, std_dev = files.mean(),files.std()
+    if Mean==0:
+        mean, std_dev = files.mean(),files.std()
+    else:
+        mean, std_dev =Mean,std 
     Max,Min= np.max(files),np.min(files)
     normalised_files=[]
     for img in files:
